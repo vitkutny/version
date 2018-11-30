01823e94e890becd7c36a077b58dd45143060b75
Milan Pála <milan.pala@peckadesign.cz>
2018-11-30 14:21:14 +0100

    Refaktoring, nový namespace, CS a PHPStan

---
+++ src/CleanCacheCommand.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types = 1);
+
+namespace Pd\Version;
+
+final class CleanCacheCommand extends \Symfony\Component\Console\Command\Command
+{
+
+	protected function configure()
+	{
+		$this->setName('pd:version:clean-cache');
+		$this->setDescription('Smaže cache verzí');
+	}
+
+
+	protected function execute(
+		\Symfony\Component\Console\Input\InputInterface $input,
+		\Symfony\Component\Console\Output\OutputInterface $output
+	) {
+		/** @var \Nette\Caching\IStorage $storage */
+		$storage = $this->getHelper('container')->getByType(\Nette\Caching\IStorage::class);
+		$conditions = [
+			'tags' => [
+				Filter::CACHE_TAG,
+			],
+		];
+
+		if ($output->getVerbosity() > \Symfony\Component\Console\Output\Output::VERBOSITY_QUIET) {
+			$output->write('Smaže se cache verzí: ');
+		}
+
+		$storage->clean($conditions);
+
+		if ($output->getVerbosity() > \Symfony\Component\Console\Output\Output::VERBOSITY_QUIET) {
+			$output->writeln('<info>Smazáno</info>');
+		}
+	}
+
+}
---
+++ src/Filter.php
@@ -0,0 +1,119 @@
+<?php declare(strict_types = 1);
+
+namespace Pd\Version;
+
+final class Filter
+{
+
+	public const CACHE_TAG = 'pd-version';
+
+	/**
+	 * @var string
+	 */
+	private $directory;
+
+	/**
+	 * @var \Nette\Http\IRequest
+	 */
+	private $request;
+
+	/**
+	 * @var \Nette\Caching\Cache|null
+	 */
+	private $cache;
+
+	/**
+	 * @var \DateTime
+	 */
+	private $expire;
+
+	/**
+	 * @var string
+	 */
+	private $parameter;
+
+	/**
+	 * @var bool
+	 */
+	private $debugMode;
+
+
+	public function __construct(
+		$directory,
+		$parameter,
+		$debugMode
+	) {
+		$this->directory = $directory;
+		$this->parameter = $parameter;
+		$this->debugMode = $debugMode;
+	}
+
+
+	public function setRequest(\Nette\Http\IRequest $request)
+	{
+		$this->request = $request;
+	}
+
+
+	public function setStorage(
+		\Nette\Caching\IStorage $storage
+	) {
+		$this->cache = new \Nette\Caching\Cache($storage, \strtr(self::class, '\\', \Nette\Caching\Cache::NAMESPACE_SEPARATOR));
+	}
+
+
+	public function __invoke(
+		$url,
+		$directory = NULL,
+		$parameter = NULL
+	) {
+		$directory = $directory ?: $this->directory;
+		$parameter = $parameter ?: $this->parameter;
+
+		$cacheCallback = function (& $dependencies) use (
+			$url,
+			$directory,
+			$parameter
+		) {
+			$dependencies[\Nette\Caching\Cache::EXPIRE] = $this->expire;
+
+			return $this->process($url, $directory, $parameter, $dependencies);
+		};
+
+		return $this->cache ? $this->cache->load([$url, $directory, $parameter, $this->debugMode], $cacheCallback) : $this->process($url, $directory, $parameter);
+	}
+
+
+	private function process(
+		$url,
+		$directory,
+		$parameter,
+		array & $dependencies = []
+	) {
+		$url = new \Nette\Http\Url($url);
+		$version = NULL;
+		if ($url->getHost() && ( ! $this->request || $url->getHost() !== $this->request->getUrl()->getHost())) {
+			$headers = @\get_headers($url, TRUE);
+			if (\is_array($headers) && isset($headers['ETag'])) {
+				$version = \preg_replace('~[^a-z0-9\-]~', '', $headers['ETag']);
+			} elseif (\is_array($headers) && isset($headers['Last-Modified'])) {
+				$version = (new \DateTime($headers['Last-Modified']))->getTimestamp();
+			}
+		} else {
+			$filename = \implode(\DIRECTORY_SEPARATOR, [
+				\rtrim($directory, '\\/'),
+				\ltrim($url->getPath(), '\\/'),
+			]);
+			if (\is_file($filename)) {
+				$version = \sha1_file($filename);
+				if ($this->debugMode) {
+					$dependencies[\Nette\Caching\Cache::FILES] = $filename;
+				}
+			}
+		}
+		$dependencies[\Nette\Caching\Cache::TAGS] = [self::CACHE_TAG];
+		$url->setQueryParameter($parameter, $version ?: \time());
+
+		return \preg_replace($pattern = '#^(\\+|/+)#', \preg_match($pattern, $url->getPath()) ? \DIRECTORY_SEPARATOR : NULL, $url);
+	}
+}
