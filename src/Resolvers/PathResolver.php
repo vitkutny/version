c7287d4612a65d185d0f1c7448debb66c2a9330b
jakubenglicky <jakubenglicky@gmail.com>
2021-03-23 11:43:30 +0100

    Podmínka nemohla být splněna
    - z podstaty fungování není proměnná $dependencies naplněna, takže podmínka se nikdy nesplnila

--- src/Resolvers/PathResolver.php
+++ src/Resolvers/PathResolver.php
@@ -50,7 +50,7 @@ class PathResolver extends \Pd\Version\Resolvers\AbstractPathResolver
 	private function process(\Nette\Http\Url $url, string $realPath, string $parameter, ?array &$dependencies = NULL): string
 	{
 		$version = (string) \sha1_file($realPath);
-		if ($this->debugMode && $dependencies) {
+		if ($this->debugMode) {
 			$dependencies[\Nette\Caching\Cache::FILES] = $realPath;
 		}
 		$dependencies[\Nette\Caching\Cache::TAGS] = \Pd\Version\Filter::CACHE_TAG;
