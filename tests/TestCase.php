d68424069558b4cde4e6c45a193e57a6a148f40d
jakubenglicky <jakubenglicky@gmail.com>
2021-03-23 12:06:48 +0100

    Refactoring testů
    - vytvořena TestCase, která vytváří kontejner
    - úprava strktury složek

---
+++ tests/TestCase.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace PdTests\Version;
+
+require_once __DIR__ . '/bootstrap.php';
+
+
+class TestCase extends \Tester\TestCase
+{
+
+	/**
+	 * @var \Nette\DI\Container
+	 */
+	protected $container;
+
+
+	public function setUp()
+	{
+		$configurator = new \Nette\Configurator();
+		$configurator->addConfig(__DIR__ . '/PdTests/Version/DI/extension.neon');
+		$configurator->setTempDirectory(__DIR__ . '/temp');
+
+		$this->container = $configurator->createContainer();
+	}
+
+}
