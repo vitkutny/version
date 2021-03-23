3db3c0185cf69142d4e766867a4282bacd7b0471
jakubenglicky <jakubenglicky@gmail.com>
2021-03-23 11:41:55 +0100

    Doplnění referencování pomocí &
    - při nastavení tagu do dependencies této proměnné chybělo referencování
    - tím pádem se sice v rámci metody tag nastavil, ale ven se již nedostal

--- src/Resolvers/PathResolver.php
+++ src/Resolvers/PathResolver.php
@@ -38,7 +38,7 @@ class PathResolver extends \Pd\Version\Resolvers\AbstractPathResolver
 			return $this->process($url, $realPath, $parameter);
 		}
 
-		return $this->cache->load([$realPath, $directory, $parameter], function (?array $dependencies) use ($url, $realPath, $parameter): string {
+		return $this->cache->load([$realPath, $directory, $parameter], function (?array &$dependencies) use ($url, $realPath, $parameter): string {
 			return $this->process($url, $realPath, $parameter, $dependencies);
 		});
 	}
@@ -47,7 +47,7 @@ class PathResolver extends \Pd\Version\Resolvers\AbstractPathResolver
 	/**
 	 * @param array<string, mixed> $dependencies
 	 */
-	private function process(\Nette\Http\Url $url, string $realPath, string $parameter, ?array $dependencies = NULL): string
+	private function process(\Nette\Http\Url $url, string $realPath, string $parameter, ?array &$dependencies = NULL): string
 	{
 		$version = (string) \sha1_file($realPath);
 		if ($this->debugMode && $dependencies) {
